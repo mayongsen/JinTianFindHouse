fdd64a3f31ea861dc0c0ef23fd755ff1824cd2b3
xgl-charon <admin@xgl6.cn>
2019-10-20 12:03:15 +0800

    配置连接池

--- src/main/java/cn/jintian/dao/BaseDao.java
+++ src/main/java/cn/jintian/dao/BaseDao.java
@@ -5,14 +5,32 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 
 public class BaseDao {
-	private String url = "jdbc:mysql://139.129.89.199:3306/jintian";
+	/*private String url = "jdbc:mysql://139.129.89.199:3306/jintian";
 	private String dbName ="root";
 	private String dbPwd = "1qaz!QAZ";
-	private static Connection conn = null;
-	
-	private BaseDao(){
+	private static Connection conn = null;*/
+
+	public static Connection getConn(){
+		Connection conn = null;
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/news");
+			conn = ds.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	/*private BaseDao(){
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(url,dbName,dbPwd);
@@ -21,13 +39,13 @@ public class BaseDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
-	public static Connection getConn(){
+	/*public static Connection getConn(){
 		if (conn == null)
 			new BaseDao();
 		return conn;
-	}
+	}*/
 	
 	public static void closeAll(ResultSet rs,Statement stat){
 		try {
